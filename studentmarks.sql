2d1d55a759f932cd0d1b1a06955a9707ec2a388b
Rohini341999 <49485663+Rohini341999@users.noreply.github.com>
2020-11-11 16:25:36 +0530

    Create studentmarks.sql

---
+++ studentmarks.sql
@@ -0,0 +1,8 @@
+create database registrationcrud;
+use reagistrationcrud;
+create table marks(id int not null primary key,
+name varchar(255) not null,
+marks int not null
+);
+
+select * from marks;
